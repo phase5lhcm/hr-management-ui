90c78e7a73320b900e27605dcce0d8daae096226
cmaynard <chrissy.swe.maynard@gmail.com>
2023-11-28 02:26:26 -0500

    add search form field to dashboard page

--- src/pages/Applications.jsx
+++ src/pages/Applications.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Applications() {
   return (
-    <div>
+    <div style={{ textAlign: "center" }}>
       <p>Hello World</p>
       <Link to="applications">Click to view your application progress</Link>
     </div>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,30 @@
-import React from "react";
-import Sidebar from "../components/Sidebar";
+import Navbar from "react-bootstrap/Navbar";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import InputGroup from "react-bootstrap/InputGroup";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function Dashboard() {
   return (
-    <div className="Dashboard">
-      <div className="navbar"></div>
-    </div>
+    <>
+      <Navbar className="bg-body-tertiary justify-content-center">
+        <Form inline>
+          <Row>
+            <Col xs="auto">
+              <Form.Control
+                type="text"
+                placeholder="Search this page"
+                className=" mr-sm-2"
+              />
+            </Col>
+            <Col xs="auto">
+              <Button type="submit">Submit</Button>
+            </Col>
+          </Row>
+        </Form>
+      </Navbar>
+    </>
   );
 }
 
