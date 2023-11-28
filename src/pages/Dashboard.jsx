f6e6b66fd573ca22d3fe7a9bc8fe2c905aacbfcc
cmaynard <chrissy.swe.maynard@gmail.com>
2023-11-28 02:58:04 -0500

    add cards to dashboard page. TODO- add layout for page responsiveness

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,6 +4,9 @@ import Button from "react-bootstrap/Button";
 import InputGroup from "react-bootstrap/InputGroup";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Badge from "react-bootstrap/Badge";
+import ListGroup from "react-bootstrap/ListGroup";
 
 function Dashboard() {
   return (
@@ -24,6 +27,103 @@ function Dashboard() {
           </Row>
         </Form>
       </Navbar>
+
+      <div
+        style={{
+          paddingLeft: "17em",
+          paddingTop: "1.5em",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-around",
+        }}
+      >
+        <Card style={{ width: "18rem" }}>
+          <Card.Body>
+            <Card.Title>Absence Rate</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: "18rem" }}>
+          <Card.Body>
+            <Card.Title>Absence Cost</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+      <div
+        className="listGroup"
+        style={{ border: "1mm ridge grey", padding: "1em" }}
+      >
+        <h2>Announcements</h2>
+        <ListGroup as="ol" numbered>
+          <ListGroup.Item
+            as="li"
+            className="d-flex justify-content-between align-items-start"
+          >
+            <div className="ms-2 me-auto">
+              <div className="fw-bold">Subheading</div>
+              Cras justo odio
+            </div>
+            <Badge bg="primary" pill>
+              14
+            </Badge>
+          </ListGroup.Item>
+          <ListGroup.Item
+            as="li"
+            className="d-flex justify-content-between align-items-start"
+          >
+            <div className="ms-2 me-auto">
+              <div className="fw-bold">Subheading</div>
+              Cras justo odio
+            </div>
+            <Badge bg="primary" pill>
+              14
+            </Badge>
+          </ListGroup.Item>
+          <ListGroup.Item
+            as="li"
+            className="d-flex justify-content-between align-items-start"
+          >
+            <div className="ms-2 me-auto">
+              <div className="fw-bold">Subheading</div>
+              Cras justo odio
+            </div>
+            <Badge bg="primary" pill>
+              14
+            </Badge>
+          </ListGroup.Item>
+          <ListGroup.Item
+            as="li"
+            className="d-flex justify-content-between align-items-start"
+          >
+            <div className="ms-2 me-auto">
+              <div className="fw-bold">Subheading</div>
+              Cras justo odio
+            </div>
+            <Badge bg="primary" pill>
+              14
+            </Badge>
+          </ListGroup.Item>
+          <ListGroup.Item
+            as="li"
+            className="d-flex justify-content-between align-items-start"
+          >
+            <div className="ms-2 me-auto">
+              <div className="fw-bold">Subheading</div>
+              Cras justo odio
+            </div>
+            <Badge bg="primary" pill>
+              14
+            </Badge>
+          </ListGroup.Item>
+        </ListGroup>
+      </div>
     </>
   );
 }
