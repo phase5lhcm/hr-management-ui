17876b6e77f084e05d97b7accc3dbedd1065bac8
cmaynard <chrissy.swe.maynard@gmail.com>
2023-12-02 21:46:38 -0500

    add navbar TODO

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,17 +7,21 @@ import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import Badge from "react-bootstrap/Badge";
 import ListGroup from "react-bootstrap/ListGroup";
+import Container from "react-bootstrap/Container";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown, faBell } from "@fortawesome/free-solid-svg-icons";
 
 function Dashboard() {
   return (
-    <>
-      <Navbar className="bg-body-tertiary justify-content-center">
-        <Form inline>
+    <Container fluid style={{ paddingLeft: "5rem" }}>
+      <Navbar className="bg-body-tertiary justify-content-between">
+        <Form inline="true">
           <Row>
             <Col xs="auto">
               <Form.Control
+                id="serachField"
                 type="text"
-                placeholder="Search this page"
+                placeholder="Search"
                 className=" mr-sm-2"
               />
             </Col>
@@ -26,105 +30,119 @@ function Dashboard() {
             </Col>
           </Row>
         </Form>
+        <Navbar.Collapse
+          className="justify-content-end"
+          style={{ paddingRight: "1rem" }}
+        >
+          <Navbar.Text style={{ paddingRight: "1rem" }}>
+            <FontAwesomeIcon icon={faBell} />
+          </Navbar.Text>
+          <Navbar.Text className="justify-content-between">
+            <span>
+              Hello Mark Otto <FontAwesomeIcon icon={faAngleDown} />
+            </span>
+          </Navbar.Text>
+        </Navbar.Collapse>
       </Navbar>
+    </Container>
 
-      <div
-        style={{
-          paddingLeft: "17em",
-          paddingTop: "1.5em",
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-around",
-        }}
-      >
-        <Card style={{ width: "18rem" }}>
-          <Card.Body>
-            <Card.Title>Absence Rate</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "18rem" }}>
-          <Card.Body>
-            <Card.Title>Absence Cost</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </div>
-      <div
-        className="listGroup"
-        style={{ border: "1mm ridge grey", padding: "1em" }}
-      >
-        <h2>Announcements</h2>
-        <ListGroup as="ol" numbered>
-          <ListGroup.Item
-            as="li"
-            className="d-flex justify-content-between align-items-start"
-          >
-            <div className="ms-2 me-auto">
-              <div className="fw-bold">Subheading</div>
-              Cras justo odio
-            </div>
-            <Badge bg="primary" pill>
-              14
-            </Badge>
-          </ListGroup.Item>
-          <ListGroup.Item
-            as="li"
-            className="d-flex justify-content-between align-items-start"
-          >
-            <div className="ms-2 me-auto">
-              <div className="fw-bold">Subheading</div>
-              Cras justo odio
-            </div>
-            <Badge bg="primary" pill>
-              14
-            </Badge>
-          </ListGroup.Item>
-          <ListGroup.Item
-            as="li"
-            className="d-flex justify-content-between align-items-start"
-          >
-            <div className="ms-2 me-auto">
-              <div className="fw-bold">Subheading</div>
-              Cras justo odio
-            </div>
-            <Badge bg="primary" pill>
-              14
-            </Badge>
-          </ListGroup.Item>
-          <ListGroup.Item
-            as="li"
-            className="d-flex justify-content-between align-items-start"
-          >
-            <div className="ms-2 me-auto">
-              <div className="fw-bold">Subheading</div>
-              Cras justo odio
-            </div>
-            <Badge bg="primary" pill>
-              14
-            </Badge>
-          </ListGroup.Item>
-          <ListGroup.Item
-            as="li"
-            className="d-flex justify-content-between align-items-start"
-          >
-            <div className="ms-2 me-auto">
-              <div className="fw-bold">Subheading</div>
-              Cras justo odio
-            </div>
-            <Badge bg="primary" pill>
-              14
-            </Badge>
-          </ListGroup.Item>
-        </ListGroup>
-      </div>
-    </>
+    //   <div
+    //     style={{
+    //       paddingLeft: "17em",
+    //       paddingTop: "1.5em",
+    //       display: "flex",
+    //       flexDirection: "row",
+    //       justifyContent: "space-around",
+    //     }}
+    //   >
+    //     <Card style={{ width: "18rem" }}>
+    //       <Card.Body>
+    //         <Card.Title>Absence Rate</Card.Title>
+    //         <Card.Text>
+    //           Some quick example text to build on the card title and make up the
+    //           bulk of the card's content.
+    //         </Card.Text>
+    //       </Card.Body>
+    //     </Card>
+    //     <Card style={{ width: "18rem" }}>
+    //       <Card.Body>
+    //         <Card.Title>Absence Cost</Card.Title>
+    //         <Card.Text>
+    //           Some quick example text to build on the card title and make up the
+    //           bulk of the card's content.
+    //         </Card.Text>
+    //       </Card.Body>
+    //     </Card>
+    //   </div>
+    //   <div
+    //     className="listGroup"
+    //     style={{ border: "1mm ridge grey", padding: "1em" }}
+    //   >
+    //     <h2>Announcements</h2>
+    //     <ListGroup as="ol" numbered>
+    //       <ListGroup.Item
+    //         as="li"
+    //         className="d-flex justify-content-between align-items-start"
+    //       >
+    //         <div className="ms-2 me-auto">
+    //           <div className="fw-bold">Subheading</div>
+    //           Cras justo odio
+    //         </div>
+    //         <Badge bg="primary" pill>
+    //           14
+    //         </Badge>
+    //       </ListGroup.Item>
+    //       <ListGroup.Item
+    //         as="li"
+    //         className="d-flex justify-content-between align-items-start"
+    //       >
+    //         <div className="ms-2 me-auto">
+    //           <div className="fw-bold">Subheading</div>
+    //           Cras justo odio
+    //         </div>
+    //         <Badge bg="primary" pill>
+    //           14
+    //         </Badge>
+    //       </ListGroup.Item>
+    //       <ListGroup.Item
+    //         as="li"
+    //         className="d-flex justify-content-between align-items-start"
+    //       >
+    //         <div className="ms-2 me-auto">
+    //           <div className="fw-bold">Subheading</div>
+    //           Cras justo odio
+    //         </div>
+    //         <Badge bg="primary" pill>
+    //           14
+    //         </Badge>
+    //       </ListGroup.Item>
+    //       <ListGroup.Item
+    //         as="li"
+    //         className="d-flex justify-content-between align-items-start"
+    //       >
+    //         <div className="ms-2 me-auto">
+    //           <div className="fw-bold">Subheading</div>
+    //           Cras justo odio
+    //         </div>
+    //         <Badge bg="primary" pill>
+    //           14
+    //         </Badge>
+    //       </ListGroup.Item>
+    //       <ListGroup.Item
+    //         as="li"
+    //         className="d-flex justify-content-between align-items-start"
+    //       >
+    //         <div className="ms-2 me-auto">
+    //           <div className="fw-bold">Subheading</div>
+    //           Cras justo odio
+    //         </div>
+    //         <Badge bg="primary" pill>
+    //           14
+    //         </Badge>
+    //       </ListGroup.Item>
+    //     </ListGroup>
+    //   </div>
+    // </>
   );
 }
 
