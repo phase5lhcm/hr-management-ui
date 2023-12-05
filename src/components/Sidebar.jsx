8bd4074b779f2d5e376c3e098fc1587a2f2b4fd8
cmaynard <chrissy.swe.maynard@gmail.com>
2023-12-05 02:13:11 -0500

    working on off canvas component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,10 +7,12 @@ import {
   faBell,
   faAngleDown,
   faPerson,
+  faHouse,
 } from "@fortawesome/free-solid-svg-icons";
 import Navbar from "react-bootstrap/Navbar";
 import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
@@ -54,20 +56,74 @@ function Sidebar() {
           </Navbar.Text>
         </Navbar.Collapse>
       </Navbar>
-
-      <Offcanvas show={show} onHide={handleClose}>
+      =
+      <Offcanvas show={show} onHide={handleClose} style={{ width: "17rem" }}>
         <Offcanvas.Header closeButton>
           <Offcanvas.Title>Offcanvas</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          <Nav defaultActiveKey="/home" className="flex-column">
-            <Nav.Link href="/home">Active</Nav.Link>
-            <Nav.Link eventKey="link-1">Link</Nav.Link>
-            <Nav.Link eventKey="link-2">Link</Nav.Link>
-            <Nav.Link eventKey="disabled" disabled>
-              Disabled
-            </Nav.Link>
-          </Nav>
+          <Container>
+            <Row>
+              <Col md={10}>
+                <Nav
+                  defaultActiveKey="/dashboard"
+                  className="flex-column"
+                  style={{}}
+                >
+                  <Nav.Link
+                    href="/dashboard"
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "space-evenly",
+                      alignContent: "center",
+                    }}
+                  >
+                    <FontAwesomeIcon
+                      icon={faHouse}
+                      style={{
+                        fontSize: "2em",
+                      }}
+                    />
+                    <span>
+                      <p
+                        style={{
+                          fontSize: "1.5em",
+                          paddingTop: ".3rem",
+                        }}
+                      >
+                        Dashboard
+                      </p>
+                    </span>
+                  </Nav.Link>
+                  <Nav.Link
+                    eventKey="link-1"
+                    href="applications"
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "space-evenly",
+                      alignContent: "center",
+                    }}
+                  >
+                    <FontAwesomeIcon
+                      icon={faPerson}
+                      style={{
+                        fontSize: "2em",
+                      }}
+                    />
+                    <span>
+                      <p style={{}}>Applicant Tracker</p>
+                    </span>
+                  </Nav.Link>
+                  <Nav.Link eventKey="link-2">Link</Nav.Link>
+                  <Nav.Link eventKey="disabled" disabled>
+                    Disabled
+                  </Nav.Link>
+                </Nav>
+              </Col>
+            </Row>
+          </Container>
         </Offcanvas.Body>
       </Offcanvas>
     </>
